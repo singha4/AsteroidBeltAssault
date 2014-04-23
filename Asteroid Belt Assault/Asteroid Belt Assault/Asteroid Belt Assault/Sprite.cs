92b34f7837d4f6f68abacca7fb50e2b806ad142a
singha4 <amandeep159632478@gmail.com>
2014-04-23 10:06:16 -0400

    version 1

---
+++ Asteroid Belt Assault/Asteroid Belt Assault/Asteroid Belt Assault/Sprite.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Asteroid_Belt_Assault
+{
+    class Sprite
+    {
+        public Texture2D Texture;
+        protected List<Rectangle> frames = new List<Rectangle>();
+        private int frameWidth = 0;
+        private int frameHeight = 0;
+        private int currentFrame;
+        private float frameTime = 0.1f;
+        private float timeForCurrentFrame = 0.0f;
+        private Color tintColor = Color.White;
+        private float rotation = 0.0f;
+        public int CollisionRadius = 0;
+        public int BoundingXPadding = 0;
+        public int BoundingYPadding = 0;
+        protected Vector2 location = Vector2.Zero;
+        protected Vector2 velocity = Vector2.Zero;
+
+        public Sprite(
+            Vector2 location,
+            Texture2D texture,
+            Rectangle initialFrame,
+            Vector2 velocity)
+        {
+            this.location = location;
+            Texture = texture;
+            this.velocity = velocity;
+            frames.Add(initialFrame);
+            frameWidth = initialFrame.Width;
+            frameHeight = initialFrame.Height;
+        }
+
+
+        public Vector2 Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+
+        public Color TintColor
+        {
+            get { return tintColor; }
+            set { tintColor = value; }
+        }
+
+
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value % MathHelper.TwoPi; }
+        }
+
+        public int Frame
+        {
+            get { return currentFrame; }
+            set { currentFrame = (int)MathHelper.Clamp(value, 0, frames.Count - 1); }
+        }
+
+        public float FrameTime
+        {
+            get { return frameTime; }
+            set { frameTime = MathHelper.Max(0, value); }
+        }
+
+        public Rectangle Source
+        {
+            get { return frames[currentFrame]; }
+        }
+        public Rectangle Destination
+        {
+            get
+            {
+                return new Rectangle(
+                (int)location.X,
+                (int)location.Y,
+                frameWidth,
+                frameHeight);
+            }
+        }
+        public Vector2 Center
+        {
+            get
+            {
+                return location +
+                new Vector2(frameWidth / 2, frameHeight / 2);
+            }
+        }
+
+
+    }
+}
